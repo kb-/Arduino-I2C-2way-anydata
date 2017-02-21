aecf6ae4719ecb53781e68540cafde76ebcb8def
kb- <fox_mhhyhq@Rcpt.at>
2017-02-21 01:32:42 +0100

    1 master 2 slaves
    Random response on slaves...

--- Master_test_i2c_data/Master_test_i2c_data.ino
+++ Master_test_i2c_data/Master_test_i2c_data.ino
@@ -4,7 +4,8 @@
 #include <Wire.h>
 #include "I2C_Anything.h"
 
-#define CTRL_I2C_ADDR 12
+#define CTRL_I2C_ADDR1 12
+#define CTRL_I2C_ADDR2 13
 
 struct __attribute__ ((packed)) ctrlcomdata {
 	char id='Y';
@@ -26,19 +27,33 @@ void setup(){
 
 void loop(){
 	ctrlcomdata data;
-	data.data = millis();
   static uint32_t t=0;
   if ((millis()-t)>2000)//delay, without delay()
   {
     t=millis();
-  	Wire.beginTransmission (CTRL_I2C_ADDR);
+  	Wire.beginTransmission (CTRL_I2C_ADDR1);
+    data.id = 'Y';
+    data.data = millis();
   	Wire.write ((uint8_t*) &data, sizeof(ctrlcomdata));
   	Wire.endTransmission (true);
+    
+    Wire.beginTransmission (CTRL_I2C_ADDR2);
+    data.id = 'E';
+    data.data = millis();
+    Wire.write ((uint8_t*) &data, sizeof(ctrlcomdata));
+    Wire.endTransmission (true);
   }
 
-  if (Wire.requestFrom(CTRL_I2C_ADDR, sizeof(ctrlcomdata))){
+  if (Wire.requestFrom(CTRL_I2C_ADDR1, sizeof(ctrlcomdata))){
     I2C_readAnything(data_from_slave);
     if(data_from_slave.id=='Y'){//empty or real data is flowing in continously, only keep real data
+      Serial.println (data_from_slave.id);
+      Serial.println (data_from_slave.data);
+    }
+  }
+  if (Wire.requestFrom(CTRL_I2C_ADDR2, sizeof(ctrlcomdata))){
+    I2C_readAnything(data_from_slave);
+    if(data_from_slave.id=='E'){//empty or real data is flowing in continously, only keep real data
       Serial.println (data_from_slave.id);
       Serial.println (data_from_slave.data);
     }
--- Slave_test_i2c_data2/Slave_test_i2c_data2.ino
+++ Slave_test_i2c_data2/Slave_test_i2c_data2.ino
@@ -4,7 +4,7 @@
 #include <Wire.h>
 #include "I2C_Anything.h"
 
-#define CTRL_I2C_ADDR 13
+#define CTRL_I2C_ADDR 12
 
 struct __attribute__ ((packed)) ctrlcomdata {
 	char id='X';
@@ -30,7 +30,7 @@ void setup()
 }
 
 void loop(){
-  if(data_ready&&data_from_Master.id=='E'){//Serial.print isn't recommanded in receiveEvent...; randon or real data is flowing in continously, only keep real data
+  if(data_ready&&data_from_Master.id=='Y'){//Serial.print isn't recommanded in receiveEvent...; randon or real data is flowing in continously, only keep real data
     Serial.println (data_from_Master.id);
     Serial.println (data_from_Master.action);
     Serial.println (data_from_Master.data);
@@ -48,7 +48,7 @@ void requestEvent(){
   if ((millis()-t)>1000)//delay, without delay()
   {
     t=millis();
-    data_to_Master.id = 'E';
+    data_to_Master.id = 'Y';
     data_to_Master.data = millis();
     Wire.write ((uint8_t*) &data_to_Master, sizeof(ctrlcomdata));
   }
