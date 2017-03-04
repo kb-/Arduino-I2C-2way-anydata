afc08a636ab545d6a24c93666d9937cf42aae083
kb- <fox_mhhyhq@Rcpt.at>
2017-03-04 21:11:31 +0100

    Collision test
    100% success from slave to master. ≈99.8 from masters to slave
    (slave update was missing)

--- Slave_test_i2c_data/Slave_test_i2c_data.ino
+++ Slave_test_i2c_data/Slave_test_i2c_data.ino
@@ -15,11 +15,18 @@ struct __attribute__ ((packed)) ctrlcomdata {
 volatile ctrlcomdata data_from_Master;
 volatile ctrlcomdata data_to_Master;
 volatile bool data_ready=false;
+volatile uint32_t ok1=0;
+volatile uint32_t ok2=0;
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
 
 //The setup function is called once at startup of the sketch
 void setup()
 {
+  pinMode(12, OUTPUT);//enable 12V
+  digitalWrite(12, LOW);//enable 12V
 	Serial.begin(115200);  // start serial for output
+  inputString.reserve(2);
 	while (!Serial) {
 	; // wait for serial port to connect. Needed for native USB
 	}
@@ -31,10 +38,28 @@ void setup()
 
 void loop(){
   if(data_ready){
-    Serial.println (data_from_Master.id);
-    Serial.println (data_from_Master.action);
-    Serial.println (data_from_Master.data);
+//    Serial.println (data_from_Master.id);
+//    Serial.println (data_from_Master.action);
+//    Serial.println (data_from_Master.data);
+      if(data_from_Master.id=='Z'&&data_from_Master.action=='e'&&data_from_Master.data==1500)
+      {
+        ok1++;
+      }else if(data_from_Master.id=='Y'&&data_from_Master.action=='e'&&data_from_Master.data==2000)
+      {
+        ok2++;
+      }
     data_ready = false;
+  }
+  if (stringComplete) {
+    if(inputString=="r\r"){
+      Serial.print(ok1);
+      Serial.println (" Valid transfers m1");
+      Serial.print(ok2);
+      Serial.println (" Valid transfers m2");      
+    }
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
   }
 }
 
@@ -47,9 +72,25 @@ void receiveEvent(int howMany) {
 
 void requestEvent(){
   static uint32_t t=0;
-    data_to_Master.data = millis();
+    data_to_Master.data = 2500;
     data_to_Master.action = 'Z';
     Wire.write ((uint8_t*) &data_to_Master, sizeof(ctrlcomdata));
+    data_to_Master.data = 3500;
     data_to_Master.action = 'Y';
     Wire.write ((uint8_t*) &data_to_Master, sizeof(ctrlcomdata));//we don't know which master is calling. Sending data fo both.
+}
+
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\r') {
+      stringComplete = true;
+    }
+  }
 }
